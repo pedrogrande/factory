e0a0042f8613587ff57481954e4bf1437c5160d4
pedroargento <peter.argent@gmail.com>
2013-09-15 14:14:46 +1000

    first commit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,33 @@
 //= require bootstrap
 //= require turbolinks
 //= require_tree .
+
+$(document).ready(function() {
+ 
+    // grab the initial top offset of the navigation 
+    var sticky_navigation_offset_top = $('#sticky_navigation').offset().top;
+     
+    // our function that decides whether the navigation bar should have "fixed" css position or not.
+    var sticky_navigation = function(){
+        var scroll_top = $(window).scrollTop(); // our current vertical position from the top
+         
+        // if we've scrolled more than the navigation, change its position to fixed to stick to top,
+        // otherwise change it back to relative
+        if (scroll_top > sticky_navigation_offset_top) { 
+            $('#sticky_navigation').css({ 'display': 'inline', 'position': 'fixed', 'top':0, 'left':0 });
+            $('#small_logo').css({ 'display': 'inline' });
+        } else {
+            $('#sticky_navigation').css({ 'display': 'none' });
+            $('#small_logo').css({ 'display': 'none' }); 
+        }   
+    };
+     
+    // run our function on load
+    sticky_navigation();
+     
+    // and run it again every time you scroll
+    $(window).scroll(function() {
+         sticky_navigation();
+    });
+ 
+});
